fab23e1d1f50300366e21375bbc58d579f5f3026
misibi <117075731+misibi@users.noreply.github.com>
2025-08-29 09:40:28 +0100

    Simplify next.config.js to fix CommonJS warnings

--- next.config.js
+++ next.config.js
@@ -1,15 +1,3 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/.well-known/farcaster.json',
-        destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/0198f30c-2098-b590-4320-fa990ff1a919',
-        permanent: false,https
-      },
-    ];
-  },
-};
-
-module.exports = nextConfig;
+}
